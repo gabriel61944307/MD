3ca85cf8c9c45293449f589927fa616d31fc1554
gabriel61944307 <48767872+gabriel61944307@users.noreply.github.com>
2019-06-05 18:29:32 -0300

    Update PDF1_Ex1.py

--- PDF1_Ex1.py
+++ PDF1_Ex1.py
@@ -1,17 +1,14 @@
-verificador = 1
-verificadorprimo = 1
-cont = 2
-primo = 1
-while(verificador==1):
+verificaprimo = 1
+primo = 2
+cont = 3
+while (primo<=10001):
 	cont+=1
-	verificadorprimo=1
-	for y in range(2, cont):
-		if(y>cont**0.5):
+	verificaprimo = 1
+	for x in range(2,cont):
+		if(x>cont**0.5):
 			break
-		elif(cont%y==0):
-			verificadorprimo=0
-	if(verificadorprimo):
-		primo+=1
-	if(primo==10001):
-		verificador=0
+		if(cont%x==0):
+			verificaprimo = 0
+	if(verificaprimo):
+			primo+=1
 print(cont)
