df2bd61034129f20572bdb16b136f97736f8b403
gabriel61944307 <48767872+gabriel61944307@users.noreply.github.com>
2019-07-07 16:52:27 -0300

    Update PDF1_Ex2.py

--- PDF1_Ex2.py
+++ PDF1_Ex2.py
@@ -1,11 +1,11 @@
-respa=0
-respb=0
-respc=0
+respa=0		#Armazena a resposta de a.
+respb=0		#Armazena a resposta de b.
+respc=0		#Armazena a resposta de c.
 for a in range(1,1000):
 	for b in range(a+1,1000):
-		c =1000-a-b
-		if(a*a+b*b==c*c):
+		c =1000-a-b		#1000 = a+b+c logo c = 1000-a-b.
+		if(a*a+b*b==c*c):	#Se a²+b²=c² armazena as respostas.
 			respa=a
 			respb=b
 			respc=c
-print(respa*respb*respc)
+print(respa*respb*respc)	#Mostra o produto de a, b e c.
