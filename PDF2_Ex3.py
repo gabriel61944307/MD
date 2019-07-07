8270eb27f50dae787f892ed136001550b17b8f80
gabriel61944307 <48767872+gabriel61944307@users.noreply.github.com>
2019-07-07 17:00:00 -0300

    Update PDF2_Ex3.py

--- PDF2_Ex3.py
+++ PDF2_Ex3.py
@@ -1,6 +1,6 @@
-a = 2**1000
+a = 2**1000	#2 elevado a 100.
 soma = 0
-while(a!=0):
-	soma = soma + a%10
-	a=a//10
-print(soma)
+while(a!=0):			#Quando o ultimo algarismo do número
+	soma = soma + a%10	#passar neste laço a variável a passará a ser 
+	a=a//10			#igual a 0, este é o momento em que o laço
+print(soma)			#se encerra.
