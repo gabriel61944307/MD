5b899ddfbe4e9c8fc7ef80ed7b47f97caece4897
thekillel <50887451+thekillel@users.noreply.github.com>
2019-07-07 17:15:01 -0300

    Update PDF1_Ex3.py

--- PDF1_Ex3.py
+++ PDF1_Ex3.py
@@ -1,14 +1,14 @@
-valor_maior_sequencia=0
+valor_maior_sequencia=0 #definindo a variavel auxiliar que salva o maior valor da sequencia temporario
 for i in range(2,1000000):
-    maior_sequencia = 1
+    maior_sequencia = 1 #definindo o valor que sera a maior sequencia
     valor = i
-    while(valor!=1):
-        if (valor%2==0):
+    while(valor!=1): #definindo o loop enquanto o valor for maior que 1, que é onde a sequencia termina
+        if (valor%2==0): #se o valor for par se divide o mesmo por 2
             valor = valor/2
-        else:
+        else: #se for impar é aplicado a formula n = 3n+1
             valor = (3*valor+1)
-        maior_sequencia = maior_sequencia+1    
-    if(maior_sequencia>valor_maior_sequencia):
+        maior_sequencia = maior_sequencia+1   #é somado mais um na maior sequencia
+    if(maior_sequencia>valor_maior_sequencia): #se a sequencia calulada anteriormente for menor que a atual, é substituido o valor da maior sequencia e atribuido o numero a i
         valor_maior_sequencia=maior_sequencia
         resposta = i
-print(resposta)  
+print(resposta)  #apresentando o valor de i que é o da maior sequencia
