30e6ceb1a541ec4ebaeaf236dd8229bdebc742ca
gabriel61944307 <48767872+gabriel61944307@users.noreply.github.com>
2019-07-07 17:04:37 -0300

    Update PDF3_Ex3.py

--- PDF3_Ex3.py
+++ PDF3_Ex3.py
@@ -1,14 +1,14 @@
 i = 0
 cont = 0
-termos = []
-for a in range(2,101):
-  for b in range(2,101):
+termos = []	#Lista termos de a elevado a b.
+for a in range(2,101):			#Estes laços em conjunto fazem a e b oscilarem
+  for b in range(2,101):		#em todas combinações posíveis.
 		cont+=1
 		c = a**b
-		termos.append(c)
-termos.sort()
+		termos.append(c)	#Adiciona c a lista de termos.
+termos.sort()	#Organiza a lista de termos de forma crescente.
 while(i<cont-1):
-	if(termos[i]==termos[i+1]):
+	if(termos[i]==termos[i+1]):	#Remove termos iguais da lista.
 		termos.pop(i)
 		cont-=1
 	else:
