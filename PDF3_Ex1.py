e605b3487dd7fe87335362e1da42dfed9f154af7
thekillel <50887451+thekillel@users.noreply.github.com>
2019-07-07 17:08:00 -0300

    Update PDF3_Ex1.py

--- PDF3_Ex1.py
+++ PDF3_Ex1.py
@@ -1,17 +1,17 @@
 somaDiv=0
-for i in range (1, 10000):
-  somaA = 0
+for i in range (1, 10000): #loop que vai de 1 até ao 10000 que é a quantidade de numeros a ser analisado
+  somaA = 0 # somaA é a soma de todos os numeros de j que dividem i com resto 0  
   for j in range (1, i):
-    if (i%j==0):    
+    if (i%j==0): # Se o resto da divisão de i por j for 0 o programa soma todos os valores de j em somaA
       somaA = somaA + j
   
-  somaB = 0
+  somaB = 0 #somaB é a soma de todos os valores de k que dividem a somaA com resto 0
       
-  for k in range(1, somaA): 
-    if (somaA%k==0):
+  for k in range(1, somaA): #loop que percorre todos divisiveis de somaA
+    if (somaA%k==0): #se a somaA tiver resto 0 com a divisão por k soma em somaB tosos os valores de k
       somaB += k
 
-  if (somaB == i and somaB != somaA):
+  if (somaB == i and somaB != somaA): #se a somaB for igual ao contador até 10000 e for diferente da somaA, soma os valores de i para ser apresentado no final
     somaDiv += i  
 
 print(somaDiv)
