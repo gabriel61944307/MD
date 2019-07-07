b2767cbf9730890c79f58d4fdf9a691c8a432985
thekillel <50887451+thekillel@users.noreply.github.com>
2019-07-07 17:20:07 -0300

    Update PDF3_Ex2.py

--- PDF3_Ex2.py
+++ PDF3_Ex2.py
@@ -1,7 +1,7 @@
-termo = 2
-fibonacci = [1, 1]
-while len(str(fibonacci[1]))<1000:
-	termo = termo + 1
-	fibonacci=[fibonacci[1], fibonacci[0]+fibonacci[1]]
+termo = 2 #atribuindo termo=2 pois os dois primeiros termos da sequencia ja estão pré-definidos
+fibonacci = [1, 1] #atribuindo o valor 1,1 na lista
+while len(str(fibonacci[1]))<1000:#enquanto o tamanho dos caracteres da sequencia de fibonacci for men or que 1000
+	termo = termo + 1 #continuando a contagem dos numeros ordinais que corresponde ao numero da sequencia de fibonacci
+	fibonacci=[fibonacci[1], fibonacci[0]+fibonacci[1]] #calculando o proximo termo da sequencia de fibonacci
 
-print (termo)	
+print (termo)#imprimindo o numero que corresponde ao numero que contem 1000 caracteres na sequencia de fibonacci
