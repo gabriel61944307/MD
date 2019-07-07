31e7bee3cab07cbf8fe256f609912912828e1af3
gabriel61944307 <48767872+gabriel61944307@users.noreply.github.com>
2019-07-07 16:56:15 -0300

    Update PDF2_Ex1.py

--- PDF2_Ex1.py
+++ PDF2_Ex1.py
@@ -1,14 +1,14 @@
 verificaprimo = 1
-sumprimo = 0
+sumprimo = 0		#Armazena a soma dos primos
 cont = 2
-while (cont<=2000000):
-	verificaprimo = 1
-	for x in range(2,cont):
+while (cont<=2000000):		#Laço que incrementa cont para que ele seja
+	verificaprimo = 1	#verificado como primo ou não.
+	for x in range(2,cont):		#Laço que decide se o número é primo ou não.
 		if(x>cont**0.5):
 			break
 		if(cont%x==0):
 			verificaprimo = 0
 	if(verificaprimo):
-			sumprimo = sumprimo + cont
-	cont+=1
+			sumprimo = sumprimo + cont 	#Se o número for primo ele é
+	cont+=1						#acrescido a variável.
 print(sumprimo)
