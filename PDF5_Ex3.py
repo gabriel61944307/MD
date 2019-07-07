0926d43407014060dc3ed71e2517ffebd91e9a41
Isaac Willian <isaacwillian3@gmail.com>
2019-07-07 04:34:11 -0300

    Atualizando Euler 5

--- PDF5_Ex3.py
+++ PDF5_Ex3.py
@@ -1,28 +1,28 @@
 # Procurada a intersecçao nas sequencias geradas, se nao achado e aumentado o tamanho da sequencia ate ser econtrado
-tam = 100000
+tam = 10000
 achou = 0
 
 while(not achou):
- T = []
+ T = [] #Vetores que armazenam a sequencia
  P = []
  H = []
- for i in range(55386,tam):
+ for i in range(286,tam): #Obtendo sequencia dos numeros Triangulares
     T.append((i*(i-1)/2))
 
- for i in range(32096,tam):
+ for i in range(166,tam): #Obtendo sequencia dos numeros Pentagonais
     P.append((i*(3*i-1)/2))
 
- for i in range(27834,tam):
+ for i in range(144,tam): #Obtendo sequencia dos numeros Hexagonais
     H.append((i*(2*i-1)))
 
 
 
- valor = set(T).intersection(H,P)
+ valor = set(T).intersection(H,P) #Obtendo a intersecção
 
- valor = list(valor)
- if(valor):
+ valor = list(valor) 
+ if(valor): #se lista for nula  entao o intervalo é aumentado e refeito o while
     achou = 1
- tam +=100000
+ tam +=10000
     
 print(valor[0])
  
