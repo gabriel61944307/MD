df63de35b75716ea4e4b214c649347fdcbd898cf
thekillel <50887451+thekillel@users.noreply.github.com>
2019-07-07 23:22:44 -0300

    Update PDF1_Ex3.py

--- PDF1_Ex3.py
+++ PDF1_Ex3.py
@@ -8,7 +8,8 @@ for i in range(2,1000000):
         else: #se for impar é aplicado a formula n = 3n+1
             valor = (3*valor+1)
         maior_sequencia = maior_sequencia+1   #é somado mais um na maior sequencia
-    if(maior_sequencia>valor_maior_sequencia): #se a sequencia calulada anteriormente for menor que a atual, é substituido o valor da maior sequencia e atribuido o numero a i
+    if(maior_sequencia>valor_maior_sequencia): #se a sequencia calulada anteriormente for menor que a atual, 
+                                               #é substituido o valor da maior sequencia e atribuido o numero a i
         valor_maior_sequencia=maior_sequencia
         resposta = i
 print(resposta)  #apresentando o valor de i que é o da maior sequencia
