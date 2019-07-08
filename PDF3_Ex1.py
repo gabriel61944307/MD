23aeb4147d4d151878ba34609135d05195b69b77
thekillel <50887451+thekillel@users.noreply.github.com>
2019-07-07 23:07:35 -0300

    Update PDF3_Ex1.py

--- PDF3_Ex1.py
+++ PDF3_Ex1.py
@@ -11,7 +11,8 @@ for i in range (1, 10000): #loop que vai de 1 até ao 10000 que é a quantidade 
     if (somaA%k==0): #se a somaA tiver resto 0 com a divisão por k soma em somaB tosos os valores de k
       somaB += k
 
-  if (somaB == i and somaB != somaA): #se a somaB for igual ao contador até 10000 e for diferente da somaA, soma os valores de i para ser apresentado no final
+  if (somaB == i and somaB != somaA): #se a somaB for igual ao contador até 10000 e for diferente da somaA,
+                                      #soma os valores de i para ser apresentado no final
     somaDiv += i  
 
 print(somaDiv)
