f1b0a0211a6f842ffcd48c69fd3767215ea6c6f5
thekillel <50887451+thekillel@users.noreply.github.com>
2019-07-07 23:08:03 -0300

    Update PDF5_Ex1.py

--- PDF5_Ex1.py
+++ PDF5_Ex1.py
@@ -9,7 +9,8 @@ for n in range(1,1002,2): #for com todos os valores usados para obter os cantos 
     maximo_por_nivel = n*n #Obtido canto superior direito
     soma_ds += maximo_por_nivel 
     if((n-1)>0):  
-            soma_es += maximo_por_nivel - (n-1) #n-1 : valores a ser subtraidos para chegar no proximo canto (Esquerdo Superior)
+            soma_es += maximo_por_nivel - (n-1) #n-1 : valores a ser subtraidos para chegar no proximo 
+                                                #canto (Esquerdo Superior)
             soma_ei += maximo_por_nivel - ((n-1)*2)
             soma_di += maximo_por_nivel - ((n-1)*3)
         
